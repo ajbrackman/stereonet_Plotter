35815791c993ad1a9335800d300c7499d2482bb8
Adam <adam.brackman.444@my.csun.edu>
2020-03-04 22:32:11 -0800

    Added parsing of quadrant entries from files, pretty strike & dip table printout in console

--- StereonetPlotter_fileInput.py
+++ StereonetPlotter_fileInput.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import mplstereonet
 import csv
-import pandas
+from tabulate import tabulate
 
 # Create equal area stereonet
 fig = plt.figure()
@@ -18,36 +18,44 @@ with open('strikesdips.csv','r') as csvfile:
     csvreader = csv.reader(csvfile)
     headers = csvreader.next() # grabs header row
     for row in csvreader:
-        strikes.append(int(row[0]))
+        strikes.append(row[0])
         dips.append((int(row[1])))
 
+# Initializing a couple things
+azimuths = []
+strikesLength = len(strikes)
+
+# Fill the azimuth array by with parse_azimuth function to check for quadrant entries
+i=0
+while i < strikesLength:
+    azimuths.append(mplstereonet.parse_azimuth(strikes[i]))
+    i += 1
+
+# Create 2D array of strikes and dips
+strikedip = zip(azimuths,dips)
+
 # Self-explanatory
-# Next step here is to recombine and display them neatly in console window
 print "Total number of measurements: %d"%(len(strikes))
-print headers[0], strikes
-print headers[1], dips
+print tabulate(strikedip, headers, tablefmt="grid")
 
 # Initialize length variables for next steps.
 # These can be compared for testing purposes
-strikesLength = len(strikes)
-dipsLength = len(dips)
+# dipsLength = len(dips)
 
-# PLACEHOLDER FOR LIST LENGTH COMPARISON
-
-#Create N variable for loop
+# Create N variable for loop
 N = len(strikes)
 
 # Generate color map for entries
 # See https://matplotlib.org/gallery/color/colormap_reference.html &
 # https://matplotlib.org/tutorials/colors/colormaps.html
-cmap = plt.cm.get_cmap("gist_rainbow", N+1)
+cmap = plt.cm.get_cmap("rainbow", N+1)
 
 # Increment from 0 to N through strikes & dips, plotting poles, planes, & rakes
 i=0
 for i in range(N):
-    ax.plane(strikes[i], dips[i], 'g-', linewidth=2, color=cmap(i))
-    ax.pole(strikes[i], dips[i], 'g^', markersize=12, color=cmap(i))
-    ax.rake(strikes, dips, -25)
+    ax.plane(azimuths[i], dips[i], 'g-', linewidth=2, color=cmap(i))
+    ax.pole(azimuths[i], dips[i], 'g^', markersize=12, color=cmap(i))
+    ax.rake(azimuths, dips, -25)
 
 #draw grid on stereonet
 ax.grid()
