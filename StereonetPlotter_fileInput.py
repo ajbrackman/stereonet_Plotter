7d564a01269d76eb76a9dbe537fd11d3d679fae9
earthandhammer <60195427+earthandhammer@users.noreply.github.com>
2020-03-05 20:28:10 -0800

    Added density contouring and more logic

--- StereonetPlotter_fileInput.py
+++ StereonetPlotter_fileInput.py
@@ -24,8 +24,9 @@ with open('strikesdips.csv','r') as csvfile:
 # Initializing a couple things
 azimuths = []
 strikesLength = len(strikes)
-dipsLength = len(dips)
+# dipsLength = len(dips) # This is unnecessary at the moment, unused
 
+# Parse all strike inputs and convert to azimuth if necessary.
 for i in range(strikesLength):
     azimuths.append(mplstereonet.parse_azimuth(strikes[i]))
 
@@ -45,12 +46,21 @@ if poles in ['N','n','no']:
     for i in range(strikesLength):
         ax.plane(azimuths[i], dips[i], 'g-', linewidth=2, color=cmap(i))
 
+# Give option to plot rakes. Say No if only plotting poles
 rakes = raw_input("Do you want to plot the rakes? (Y/N)")
 if rakes in ['Y','y','yes']:
-    ax.rake(azimuths, dips, -25) # Plot the rakes (needs to be done outside loop)
+    ax.rake(azimuths, dips, -25)
 if rakes in ['N','n','']:
     print "I won't plot the rakes"
 
+# Gives option to contour poles
+do_contour = raw_input("Do you want to contour the poles? (Y/N)")
+if do_contour in ['Y','y','yes']:
+    cax = ax.density_contourf(azimuths,dips, measurement='poles', cbar='rainbow') #cbar arg doesn't seem to do anything
+    fig.colorbar(cax)
+if do_contour in ['N','n','no']:
+    print "I won't plot a contour map"
+    
 # Create 2D array of strikes and dips
 strikedip = zip(azimuths,dips)
 
@@ -58,15 +68,14 @@ strikedip = zip(azimuths,dips)
 print "Total number of measurements: %d"%(len(strikes))
 print tabulate(strikedip, headers, tablefmt="grid")
 
+# Draw grid on stereonet and show plot
 ax.grid()
+plt.ion()
 plt.show()
-
-
-
+plt.pause(0.01)
+raw_input("Press ENTER to continue")
 
 #plt.show() #this one doesn't work, not sure how to re-open matplotlib window...
-
-
 
 #draw grid on stereonet
 #ax.grid()
--- docs/README.md
+++ docs/README.md
@@ -6,7 +6,7 @@ Two versions currently exist:
 ## StereonetPlotter_fileinput.py
 This script takes a CSV (strikesdips.csv) with strikes (in quadrant or azimuth) format and dip values (in degrees), prints these to the console and plots planes and poles on an equal area stereonet.
 
-This version now has some very basic logic. User can choose what data to plot (poles, planes +- rakes). Not very robust yet at all.
+This version now has some very basic logic. Can choose which data to plot and can contour poles (don't try to contour planes!)
 
 ## StereonetPlotter_SingleInput.py
 This script prompts the user to enter a strike (in quadrant or azimuth) and a dip value (in degrees) and then plots the plane and pole on an equal area stereonet using a new color for each entry.
