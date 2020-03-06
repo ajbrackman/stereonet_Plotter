80ebf86c21d279f176af4d20ef1cd872b59da719
earthandhammer <60195427+earthandhammer@users.noreply.github.com>
2020-03-05 17:48:56 -0800

    Added some basic logic. Can now choose what to plot. Not very robust yet.

--- StereonetPlotter_fileInput.py
+++ StereonetPlotter_fileInput.py
@@ -24,20 +24,32 @@ with open('strikesdips.csv','r') as csvfile:
 # Initializing a couple things
 azimuths = []
 strikesLength = len(strikes)
+dipsLength = len(dips)
+
+for i in range(strikesLength):
+    azimuths.append(mplstereonet.parse_azimuth(strikes[i]))
 
 # Generate color map for entries
 # See https://matplotlib.org/gallery/color/colormap_reference.html &
 # https://matplotlib.org/tutorials/colors/colormaps.html
-cmap = plt.cm.get_cmap("rainbow", strikesLength+1)
+cmap = plt.cm.get_cmap("hsv", strikesLength+1)
 
-# Parse entries to azimuth and write to list.
-# Plot each plane and pole on the stereonet
-for i in range(strikesLength):
-    azimuths.append(mplstereonet.parse_azimuth(strikes[i]))
-    ax.plane(azimuths[i], dips[i], 'g-', linewidth=2, color=cmap(i))
-    ax.pole(azimuths[i], dips[i], 'g^', markersize=12, color=cmap(i))
+# Plot poles or planes per user input
+poles = raw_input("Do you want to plot poles instead of planes? (Y/N): ")
+if poles in ['Y','y','yes']:
+    i=0
+    for i in range(strikesLength):
+        ax.pole(azimuths[i], dips[i], 'g^', marker='o', markersize=8, color=cmap(i))
+if poles in ['N','n','no']:
+    i=0
+    for i in range(strikesLength):
+        ax.plane(azimuths[i], dips[i], 'g-', linewidth=2, color=cmap(i))
 
-ax.rake(azimuths, dips, -25) # Plot the rakes (needs to be done outside loop)
+rakes = raw_input("Do you want to plot the rakes? (Y/N)")
+if rakes in ['Y','y','yes']:
+    ax.rake(azimuths, dips, -25) # Plot the rakes (needs to be done outside loop)
+if rakes in ['N','n','']:
+    print "I won't plot the rakes"
 
 # Create 2D array of strikes and dips
 strikedip = zip(azimuths,dips)
@@ -46,8 +58,18 @@ strikedip = zip(azimuths,dips)
 print "Total number of measurements: %d"%(len(strikes))
 print tabulate(strikedip, headers, tablefmt="grid")
 
-#draw grid on stereonet
 ax.grid()
-
-#display plot
 plt.show()
+
+
+
+
+#plt.show() #this one doesn't work, not sure how to re-open matplotlib window...
+
+
+
+#draw grid on stereonet
+#ax.grid()
+#display plot
+#plt.show()
+#print "Do you want to plot anything else?"
